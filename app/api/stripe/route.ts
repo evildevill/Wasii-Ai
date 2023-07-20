d96a415b51fda6ad5723967023cdb1eb3ced8b9b
Waseem Akram <hackerbad669@gmail.com>
2023-07-20 19:12:26 +0500

    initial commit

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -43,10 +43,10 @@ export async function GET() {
           price_data: {
             currency: "USD",
             product_data: {
-              name: "Wasii AI Pro",
+              name: "Genius Pro",
               description: "Unlimited AI Generations"
             },
-            unit_amount: 25,
+            unit_amount: 25.99,
             recurring: {
               interval: "month"
             }
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import './globals.css'
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Wasii AI',
+  title: 'Wasii Ai',
   description: 'AI Platform',
 }
 
